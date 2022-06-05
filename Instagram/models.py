fecfd0b0694282d0c7905d70e308f1ae0c4e76f6
Wamaitha <wamaithaweru19@gmail.com>
2022-06-06 02:05:40 +0300

    create Profile,Post,Like,Comments model

---
+++ Instagram/models.py
@@ -0,0 +1,67 @@
+from calendar import c
+from tkinter import CASCADE
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Profile(models.Model):
+    name = models.CharField(null=True,max_length=50)
+    user = models.OneToOneField(User, related_name='profile',on_delete=models.CASCADE,default='DEFAULT VALUE', blank=True, null=True)
+    username=models.CharField(null=True,max_length=50)
+    bio=models.TextField(null=True,max_length=200)
+    avatar = models.ImageField(default='default.jpg', upload_to='profile_images',null=True, blank=True)
+    
+    def save_profile(self):
+        self.save()
+    
+    def delete_profile(self): 
+        self.delete()
+  
+
+    @classmethod
+    def search_by_name(cls,searched_term):
+        profile = cls.objects.filter(name__icontains=searched_term)
+        return profile
+
+
+    def __str__(self):
+        return self.user.username
+
+class Post(models.Model):
+    name = models.CharField(null=True,max_length=50)
+    id = models.CharField(null=False,max_length=50,primary_key=True)
+    caption = models.TextField(null=True,max_length=50)
+    image = models.ImageField(default='default.jpg', upload_to='photo_images')
+    likes = models.ManyToManyField(User,default=None, blank=True)
+    comment = models.ManyToManyField(User,default=None, blank=True,related_name='comments')
+    updated=models.DateTimeField(auto_now=True)
+    created=models.DateTimeField(auto_now=True)
+    profile=models.ForeignKey(Profile, on_delete=models.CASCADE,null=True)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def nun_likes(self):
+        return self.liked.all().count()
+# create tuple
+
+
+class Like(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE, null=True)
+    post=models.ForeignKey(Post,on_delete=models.CASCADE)
+    
+
+    def __str__(self):
+        return self.post
+
+
+class Comments(models.Model):
+    user = models.OneToOneField(User, related_name='user',on_delete=models.CASCADE,default='DEFAULT VALUE', blank=True, null=True)
+    post=models.ForeignKey(Post,on_delete=models.CASCADE)
+    posted=models.DateTimeField(auto_now=True)
+    text=models.TextField(null=True,max_length=50)
+
+    def __str__(self):
+        return self.profile
