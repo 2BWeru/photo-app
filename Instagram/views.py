25cc456e5fb63976dd3accc195b17c6867bc8367
Wamaitha <wamaithaweru19@gmail.com>
2022-06-07 12:28:44 +0300

    edit profile,home,comments function

--- Instagram/views.py
+++ Instagram/views.py
@@ -2,10 +2,12 @@ from email.message import EmailMessage
 from pyexpat.errors import messages
 from urllib import request
 from django.conf import settings
+from django.forms import ImageField
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
+from flask_login import user_logged_in
 from urllib3 import HTTPResponse
 from .models import Post, Profile,Comments
 from .email import send_welcome_email
@@ -13,11 +15,20 @@ from django.template.loader import render_to_string
 
 
 # Create your views here.
+# def index(request):
+
+#   texts= Profile.objects.all()
+
+#   return render(request,"index.html",{"texts":texts})
+
+
 def home(request):
-  updates= Post.objects.all()
-  comment = Comments.objects.filter()
   user = request.user
+  updates= Post.objects.all()
+  comment = Comments.objects.all()
+  
   texts= Profile.objects.all()
+  
 
   context = {
    'updates':updates,
@@ -29,28 +40,27 @@ def home(request):
 
 
 # imagedetails
-def details(request):
+def show_comments(request,id):
 
-  updates= Post.objects.all()
-  comment = Comments.objects.all()
+  comment= Comments.objects.filter(id=id)
   
+  return render(request,"display_comments.html",{"comment":comment})
 
+def details(request,id):
 
-  context = {
-   'comment':comment,
-   'updates':updates,
-   
-   }
-
-  return render(request,"details.html",context)
+  updates= Post.objects.filter(id=id)
+  
+  return render(request,"details.html",{"updates":updates})
 
 @login_required(login_url='/accounts/login/')
-def profile(request):
-
-  texts= Profile.objects.all()
-  context = {'texts':texts}
-
-  return render(request, 'Profile/profile.html',context)
+def profile(request,id):
+ 
+       
+  profiles = Profile.objects.filter(id=id)
+  updates= Post.objects.all().order_by('image')
+  
+  
+  return render(request, 'Profile/profile.html',{"updates":updates,"profiles":profiles})
 
 
 @login_required(login_url='/accounts/login/')
@@ -58,6 +68,7 @@ def editprofile(request):
    if request.method == "POST":
         prod = Profile()
         prod.name = request.POST.get('name')
+        prod.id = request.POST.get('id')
         prod.username = request.POST.get('username')
         prod.bio = request.POST.get('bio')
 
@@ -66,6 +77,7 @@ def editprofile(request):
 
         prod.save()
         # messages.success(request, "Profile Updated Successfully")
+        
         return redirect('users-profile')
 
    return render(request, 'Profile/edit_profile.html')
@@ -74,6 +86,7 @@ def post(request):
   if request.method == "POST":
         prod = Post()
         prod.name = request.POST.get('name')
+        prod.id = request.POST.get('id')
         prod.caption = request.POST.get('caption')
         
         if len(request.FILES) != 0:
@@ -93,7 +106,8 @@ def comments(request):
 
           prod.save()
           # messages.success(request, "Product Added Successfully")
-          return redirect('home')
+ 
+          return redirect('show_comments')
   return render(request, 'addcomment.html')
 
 # likes
