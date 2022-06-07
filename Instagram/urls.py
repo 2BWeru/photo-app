a5c67ba7f01d8a8c1c2124556bc564c14ba75487
Wamaitha <wamaithaweru19@gmail.com>
2022-06-07 20:23:21 +0300

    edit url path

--- Instagram/urls.py
+++ Instagram/urls.py
@@ -1,7 +1,6 @@
 from django import views
 from django.conf import settings
-from django.contrib import admin
-from django.urls import include, path
+from django.conf.urls import url
 from django.contrib.auth.views import LogoutView
 from django.conf.urls.static import static
 from . import views
@@ -10,17 +9,17 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.home,name = 'home'),
-    path('profile/<id>',views.profile,name = 'users-profile'),
-    path('edit_profile/',views.editprofile,name = 'edit_profile'),
-    path('post/',views.post,name = 'post'),
-    path('show_comments/<id>',views.show_comments,name = 'show_comments'),
-    path('like/<int:pk>',views.like_post,name = 'like'),
-    path('comments/',views.comments,name = 'comments'),
-    path('logout/', LogoutView.as_view(),  name='logout'),
-    path('items/',views.search,name = 'search'),
-    path('image/<id>',views.details,name = 'details'),
-    path('type/<int:id>',views.show,name = 'show-profiles'),
+    url('',views.home,name = 'home'),
+    url('profile/<id>',views.profile,name = 'users-profile'),
+    url('edit_profile/',views.editprofile,name = 'edit_profile'),
+    url('post/',views.post,name = 'post'),
+    url('show_comments/<id>',views.show_comments,name = 'show_comments'),
+    url('like/<int:pk>',views.like_post,name = 'like'),
+    url('comments/',views.comments,name = 'comments'),
+    url('logout/', LogoutView.as_view(),  name='logout'),
+    url('items/',views.search,name = 'search'),
+    url('image/<id>',views.details,name = 'details'),
+    url('type/<int:id>',views.show,name = 'show-profiles'),
        
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
